4ad4ae73598b94c5d77a2450f938b0c72763d61a
Avinash Jha <86429000+akj2002276@users.noreply.github.com>
2025-10-08 18:39:53 +0530

    Create Q1.java

---
+++ Loop/Easy Problems/Q1.java
@@ -0,0 +1,10 @@
+// Print the sum of first N natural numbers.
+
+class Main {
+ public static void main(String[] args) {
+ int N = 10;
+ for (int i = 1; i <= N; i++) {
+ System.out.print(i + " ");
+ }
+ }
+}
