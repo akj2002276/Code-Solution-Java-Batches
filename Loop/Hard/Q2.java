4a506eed5ab28fc9c29585962bd424ef590f2445
Avinash Jha <86429000+akj2002276@users.noreply.github.com>
2025-10-08 19:22:17 +0530

    Create Q2.java

---
+++ Loop/Hard/Q2.java
@@ -0,0 +1,23 @@
+//Prime fazctor of N
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++){
+            if(n % i == 0){
+               int count = 0;
+               for(int j = 1; j<=i; j++){
+                   if(i % j == 0){
+                       count++;
+                   }
+               }
+               if(count == 2){
+                   System.out.println(i);
+               }
+                
+            }
+        }
+    }
+}
