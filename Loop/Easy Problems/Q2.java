3b3d9f7f58c4d048fb916a45f6c78ae0c34a5697
Avinash Jha <86429000+akj2002276@users.noreply.github.com>
2025-10-08 18:42:15 +0530

    Update Q2.java

--- Loop/Easy Problems/Q2.java
+++ Loop/Easy Problems/Q2.java
@@ -1,1 +1,11 @@
-
+// Print the sum of first N natural numbers
+class Main {
+ public static void main(String[] args) {
+ int N = 10;
+ int sum = 0;
+ for (int i = 1; i <= N; i++) {
+ sum = sum + i;
+ }
+ System.out.println("Sum = " + sum);
+ }
+}
