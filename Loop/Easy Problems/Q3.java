65f30282f691357213dabec1d2f8ee59afaf821d
Avinash Jha <86429000+akj2002276@users.noreply.github.com>
2025-10-08 18:42:54 +0530

    Update Q3.java

--- Loop/Easy Problems/Q3.java
+++ Loop/Easy Problems/Q3.java
@@ -1,1 +1,12 @@
 
+// Print the factorial of a number
+class Main {
+ public static void main(String[] args) {
+ int N = 5;
+ int fact = 1;
+ for (int i = 1; i <= N; i++) {
+ fact = fact * i;
+ }
+ System.out.println("Factorial = " + fact);
+ }
+}
