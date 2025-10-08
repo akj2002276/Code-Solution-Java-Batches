edd221f12c2e47ef5a306e00ce67cac133b7dbca
Avinash Jha <86429000+akj2002276@users.noreply.github.com>
2025-10-08 18:55:55 +0530

    Create Q7.java

---
+++ Loop/Easy Problems/Q7.java
@@ -0,0 +1,9 @@
+ // Print all even numbers up to N
+class Main {
+ public static void main(String[] args) {
+ int N = 20;
+ for (int i = 2; i <= N; i = i + 2) {
+ System.out.print(i + " ");
+ }
+ }
+}
